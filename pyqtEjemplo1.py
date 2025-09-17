2ef9f009af185081c20b9da17f1d6d1512755c82
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-17 11:57:58 -0600

    Trabajo en layouts

--- pyqtEjemplo1.py
+++ pyqtEjemplo1.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QRadioButton, QVBoxLayout)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QRadioButton, QVBoxLayout,QLineEdit,QSpinBox,QDoubleSpinBox)
 import sys
 
 app = QApplication(sys.argv)
@@ -9,12 +9,18 @@ boton1 = QPushButton("Haz click aqui")
 radio1 = QRadioButton("Opcion 1")
 radio2 = QRadioButton("Opcion 2")
 radio3 = QRadioButton("Opcion 3")
+lineEdit1 = QLineEdit()
+spin1 = QSpinBox()
+dspin1 = QDoubleSpinBox()
 
 layout = QVBoxLayout()  # Define the layout
 layout.addWidget(boton1)
 layout.addWidget(radio1)
 layout.addWidget(radio2)
 layout.addWidget(radio3)
+layout.addWidget(lineEdit1)
+layout.addWidget(spin1)
+layout.addWidget(dspin1)
 ventana.setLayout(layout)  # Set the layout on the window
 
 ventana.show()
