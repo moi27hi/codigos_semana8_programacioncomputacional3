91c8cbd9d2340619ebbdacc1a9e7cd06763760da
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-17 11:50:57 -0600

    Creacion de radioButton 1

--- pyqtEjemplo1.py
+++ pyqtEjemplo1.py
@@ -1,10 +1,21 @@
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QRadioButton, QVBoxLayout)
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,QLineEdit)
 
 app = QApplication(sys.argv)
 ventana = QWidget()
 ventana.setWindowTitle("Mi primera ventana con PyQt5")
-ventana.setGeometry(200,200,600,400)  # tiene cuatro parametros (posicion en el eje x, posicion en el eje y, ancho, alto)
+ventana.setGeometry(200, 200, 600, 400)  # (x, y, width, height)
+boton1 = QPushButton("Haz click aqui")
+radio1 = QRadioButton("Opcion 1")
+radio2 = QRadioButton("Opcion 2")
+radio3 = QRadioButton("Opcion 3")
+
+layout = QVBoxLayout()  # Define the layout
+layout.addWidget(boton1)
+layout.addWidget(radio1)
+layout.addWidget(radio2)
+layout.addWidget(radio3)
+ventana.setLayout(layout)  # Set the layout on the window
 
 ventana.show()
 sys.exit(app.exec_())
