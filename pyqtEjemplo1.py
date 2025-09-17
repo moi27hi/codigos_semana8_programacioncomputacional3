edf417c0c1e39044691eb7676df77f431a93f7d4
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-17 11:14:58 -0600

    Layout

--- pyqtEjemplo1.py
+++ pyqtEjemplo1.py
@@ -1,7 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget;
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,QLineEdit)
 
 app = QApplication(sys.argv)
 ventana = QWidget()
+ventana.setWindowTitle("Mi primera ventana con PyQt5")
+ventana.setGeometry(200,200,600,400)  # tiene cuatro parametros (posicion en el eje x, posicion en el eje y, ancho, alto)
+
 ventana.show()
 sys.exit(app.exec_())
